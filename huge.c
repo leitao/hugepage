0197c40a1ed7bd3d42c74fdab430e41bda6166ab
Breno Leitao <leitao@debian.org>
2023-07-20 07:46:44 -0700

    Make it faster to reproduce

--- huge.c
+++ huge.c
@@ -6,34 +6,23 @@
 #include <sys/types.h>
 #include <assert.h>
 
-
-#define REALLOC 0
-
-#define SIZE (1 << 21)
-char *c = NULL;
-
 int usleep(useconds_t usec);
 
-void *alloc_huge_page(void *unused)
+#define SIZE (1 << 21)
+#define INLOOP_SIZE 100
+
+char *c = NULL;
+
+void *touch(void *unused)
 {
-#ifdef REALLOC
-	c = mmap(NULL, SIZE, PROT_READ | PROT_WRITE,
-		 MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB,
-		 -1, 0);
-#endif
-
-	if ((long unsigned) c == -1) {
-		/* perror("Failed to allocate\n"); */
-		c =  NULL;
-		return NULL;
-	}
 	char *ptr = (char *)c;
-	ptr[0] = '.';
-
 	if (!ptr) {
 		fprintf(stderr, "Failed to allocate memory\n");
 		perror("");
 	}
+
+	for (int i = 0; i < INLOOP_SIZE; i++)
+		ptr[0] = '.';
 
 	return NULL;
 }
@@ -44,19 +33,8 @@ void *mfree(void *unused)
 	if (!c)
 		return NULL;
 
-	madvise(c, SIZE, MADV_DONTNEED);
-
-	return NULL;
-}
-
-void *touch(void *unused)
-{
-	usleep(rand() % 300);
-	if (!c)
-		return NULL;
-
-	char x = ((char *)c)[0];
-	printf("%c", x);
+	for (int i = 0; i < INLOOP_SIZE; i++)
+		madvise(c, SIZE, MADV_DONTNEED);
 
 	return NULL;
 }
@@ -67,24 +45,26 @@ int main()
 
 	srand(getpid());
 
+
 	while (1) {
-		pthread_create(&thread1, NULL, alloc_huge_page, NULL);
-		pthread_create(&thread2, NULL, mfree, NULL);
-		pthread_create(&thread3, NULL, touch, NULL);
+		c = mmap(NULL, SIZE, PROT_READ | PROT_WRITE,
+			 MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB,
+			 -1, 0);
+
+		if ((long unsigned) c == -1) {
+			perror("Failed to allocate\n");
+			continue;
+		}
+
+		pthread_create(&thread1, NULL, mfree, NULL);
+		pthread_create(&thread2, NULL, touch, NULL);
 
 		pthread_join(thread1, NULL);
 		pthread_join(thread2, NULL);
-		pthread_join(thread3, NULL);
-
-#ifdef REALLOC
 		munmap(c, SIZE);
-#endif
-		c = NULL;
-		/* fprintf(stderr, "."); */
+		fprintf(stderr, ".");
 	}
 
-	fprintf(stderr, "Waiting thread1\n");
-	fprintf(stderr, "Waiting thread2\n");
 
 	getchar();
 
