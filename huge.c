35783eedf1d1b5b92af84fbe545fce9b973fd352
Breno Leitao <leitao@debian.org>
2023-08-21 08:41:12 -0700

    ftrace clear

--- huge.c
+++ huge.c
@@ -5,13 +5,19 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <assert.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 
 int usleep(useconds_t usec);
 
 #define SIZE (1 << 21)
-#define INLOOP_SIZE 100
+#define INLOOP_SIZE 1000
 
 char *c = NULL;
+int marker;
 
 void *touch(void *unused)
 {
@@ -19,32 +25,46 @@ void *touch(void *unused)
 	if (!ptr) {
 		fprintf(stderr, "Failed to allocate memory\n");
 		perror("");
+		exit(2);
 	}
 
-	for (int i = 0; i < INLOOP_SIZE; i++)
+	for (int i = 0; i < INLOOP_SIZE; i++) {
 		ptr[0] = '.';
+	}
 
 	return NULL;
 }
 
 void *mfree(void *unused)
 {
-	usleep(rand() % 10);
 	if (!c)
 		return NULL;
 
-	for (int i = 0; i < INLOOP_SIZE; i++)
+	for (int i = 0; i < INLOOP_SIZE; i++) {
+		/* write(marker, "madvise", 6); */
 		madvise(c, SIZE, MADV_DONTNEED);
+	}
 
 	return NULL;
+}
+
+void clear_ftrace(int fd)
+{
+	int value = 1;
+
+	if (fd < 0) {
+		perror("Failed to clear ftrace\n");
+	}
+	write(fd, &value, sizeof(value));
+	close(fd);
 }
 
 int main()
 {
 	pthread_t thread1, thread2, thread3;
 
-	srand(getpid());
-
+	int buffer = open("/sys/kernel/tracing/free_buffer", O_WRONLY);
+	marker = open("/sys/kernel/tracing/trace_marker", O_WRONLY);
 
 	while (1) {
 		c = mmap(NULL, SIZE, PROT_READ | PROT_WRITE,
@@ -56,17 +76,15 @@ int main()
 			continue;
 		}
 
+		/* clear_ftrace(buffer); */
 		pthread_create(&thread1, NULL, mfree, NULL);
 		pthread_create(&thread2, NULL, touch, NULL);
 
 		pthread_join(thread1, NULL);
 		pthread_join(thread2, NULL);
 		munmap(c, SIZE);
-		fprintf(stderr, ".");
+		printf(".");
 	}
-
-
-	getchar();
 
 }
 
